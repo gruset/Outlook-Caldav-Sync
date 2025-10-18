aaff8f4d3a616c978cd8e285a737df7f9f9ae782
Steen Kjøng Kristensen <4093594+gruset@users.noreply.github.com>
2025-10-18 21:07:12 +0200

    Updated config for auth and number of events to fetch

--- config.py
+++ config.py
@@ -3,15 +3,18 @@ import datetime as dt
 ## Generic variables
 debug = False # Set to true for more detaild logging
 LogToFile = True # Set to true for logging to file
-START_TIME = dt.date.today() # Use today as starting date
+today = dt.date.today()
+#START_TIME = today - dt.timedelta(days=365) # Use 365 days back as starting date
+START_TIME = today # Use today as starting date
 END_TIME = START_TIME + dt.timedelta(days=365) # Add a 365 days to starting date
+#END_TIME = today + dt.timedelta(days=365) # Use today + 365 days as end date
 
 ## O365 Variables
 CLIENT_ID = "ClientId"
 CLIENT_SECRET = "ClientSecret"
 TENANT_ID = "TenantId"
 DEFAULT_RESOURCE = "default@calendar.com"
-O365_FORCE_AUTHENTICATION = False # Set to True to force user authentication. Only used for initial login or if refresh token is expired.
+AUTHENTICATE_CONSOLE = False # Set this to True to force interactive console authentication
 
 ## iCloud Variables
 caldav_url = "https://calendar.icloud.com/"
--- outlook_icloud_calendar_sync.py
+++ outlook_icloud_calendar_sync.py
@@ -73,13 +73,7 @@ credentials = (CLIENT_ID, CLIENT_SECRET)
 protocol = MSGraphProtocol(default_resource=conf.DEFAULT_RESOURCE)
 account = Account(credentials, tenant_id=TENANT_ID, protocol=protocol)
 
-### Authentication logic ###
-"""
-The auth logic is outcommented. Only required for initial authentication on "delegated" permissions.
-We use the refresh token to maintain the connection.
-"""
-
-if conf.O365_FORCE_AUTHENTICATION:
+if conf.AUTHENTICATE_CONSOLE:
     LogToConsole("Force O365 authentication set to true, authenticating...")
     scopes = [ 'basic', 'calendar' ]
     if account.authenticate(scopes=scopes):
@@ -92,7 +86,7 @@ calendar = schedule.get_default_calendar()
 
 q = calendar.new_query('start').greater_equal(START_TIME)
 q.chain('and').on_attribute('end').less_equal(END_TIME)
-events = calendar.get_events(query=q, include_recurring=True)
+events = calendar.get_events(query=q, include_recurring=True, limit=1000)
 
 # Create a class to hold O365 Event data
 class O365Event:
